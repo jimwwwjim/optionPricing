397a001cd3c2259bfa69bd282e17e3d0e665734e
JUNYI  WANG <499123398@qq.com>
2019-10-17 15:58:51 +0800

    back to working on pricing

--- MonteCarlo_AsianOption.py
+++ MonteCarlo_AsianOption.py
@@ -56,15 +56,41 @@ def MonteCarlo_2(reTime,rf,S,K,sigma): #special for price insurance, whole perio
 	return {'asianput_MC':p,'asiancall_MC':c}
 
 
+def MonteCarlo_3(reTime,rf,S,K,sigma): #special for price insurance, whole period price average
+	reTime = reTime     #no necessary in this case
+	siTi = 1000000
+	list = []   #asian put option value list
+	dt = 1/360
+	totalNodes = reTime
+	for si in range(siTi):
+		path = [S]
+		for node in range(int(totalNodes)-1):
+			path.append(path[-1]*exp((rf-0.5*sigma**2)*dt+(sigma*sqrt(dt)*gauss(0,1))))
+		if min(path) > 0.97*K:
+			executive_price = path[-1]
+		elif min(path)>0.935*K:
+			executive_price = min(path[-1],0.97*K)
+		elif min(path)<0.935*K:
+			executive_price = 0.935*K
+		asian_put_value = max(K-executive_price,34.1)
+		list.append(asian_put_value)
+	p = sum(list)/siTi
+	return {'asianput ratio:',p/S,
+			'asianput value:',p}
+
 # paras input and result
-reTime = 122     #remaing days, can be modified in the time structure
-rf = .028     # risk free rate
-S = 3400     # current price
-K = 3400     # strike price
-sigma = .18    # volatility, mostly the delta of option based on this para
+reTime = 60     #remaing days, can be modified in the time structure
+rf = .0305     # risk free rate
+S = 7583    # current price
+K = S    # strike price
+sigma = .27    # volatility, mostly the delta of option based on this para
+print('remaing time:',reTime)
+print('risk free rate:',rf)
+print('initial price:',S)
+print('target price:',K)
+print('*** volatility***',sigma)
 timer_ = perf_counter()
-print('--------最后三十天取平均值--------------------')
-print(MonteCarlo_1(reTime,rf,S,K,sigma), perf_counter()-timer_)
+print('--------增强亚式   0.97/0.935--------------------')
+print(MonteCarlo_3(reTime,rf,S,K,sigma))
+print('time cosuming:',perf_counter()-timer_)
 timer_ = perf_counter()
-print('--------整个阶段取平均值--------------------')
-print(MonteCarlo_2(reTime,rf,S,K,sigma), perf_counter()-timer_)
